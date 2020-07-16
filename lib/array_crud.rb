9ecde31b19fb81b9aa2098fad967adfaf49770ad
jade decordova <lacroixjaded@gmail.com>
2020-07-16 11:33:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [a.b.c.d]
+  array = ['a','b','c','d']
 end
 
 def add_element_to_end_of_array(array, element)
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-   array.unshift(alement)
+   array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -32,7 +32,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  dex = array.lenght - 1 
+  dex = array.length - 1 
   array[dex]
 end
 
